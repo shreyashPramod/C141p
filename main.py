2448ef490e75661d9b5d3682a91b0f31fd3a609e
shreyashPramod <65612631+shreyashPramod@users.noreply.github.com>
2022-03-25 05:50:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask,jasonify,request
+import csv
+all_articles=[]
+with open("articles.csv") as f:
+    reader=csv.reader(f)
+    data=list(reader)
+    all_articles=data[1:]
+
+liked_articles=[]
+not_liked_articles=[]
